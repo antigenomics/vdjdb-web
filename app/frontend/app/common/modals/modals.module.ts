a1c0ad704760acb3f3b6dc048edb0fa70284d149
Dmitry <bvdmitri@gmail.com>
2017-09-09 15:05:58 +0300

    dep update

--- app/frontend/app/common/modals/modals.module.ts
+++ app/frontend/app/common/modals/modals.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { TooltipDirective } from "./tooltip.component";
-import { TooltipContentComponent } from "./tooltip-content.component";
 import { PopupDirective } from "./popup.directive";
 
 @NgModule({
     imports:         [ BrowserModule, FormsModule ],
-    declarations:    [ TooltipDirective, TooltipContentComponent, PopupDirective ],
-    exports:         [ TooltipDirective, TooltipContentComponent, PopupDirective ],
-    entryComponents: [ TooltipContentComponent ]
+    declarations:    [ PopupDirective ],
+    exports:         [ PopupDirective ]
 })
 export class ModalsModule {}
