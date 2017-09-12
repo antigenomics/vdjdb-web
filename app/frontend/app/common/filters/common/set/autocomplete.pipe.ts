ffb77f3c9cd4d8837e147e00af0009e39bbfd9aa
Dmitry <bvdmitri@gmail.com>
2017-09-12 19:32:11 +0300

    new set

--- app/frontend/app/common/filters/common/set/autocomplete.pipe.ts
+++ app/frontend/app/common/filters/common/set/autocomplete.pipe.ts
@@ -1,28 +1,20 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { isUndefined } from "util";
-import { SetComponent } from "./set.component";
 
 @Pipe({
     name: 'autocomplete'
 })
 export class AutocompletePipe implements PipeTransform {
-    static maxElements: number = 5;
 
     transform(values: string[], set: string): string[] {
-        if (isUndefined(set)) return AutocompletePipe.addTriplePoints(values);
-        if (set === '') return AutocompletePipe.addTriplePoints(values);
+        if (isUndefined(set)) return values;
+        if (set === '') return values;
         let setValues: string[] = set.split(',');
         let lastSetValue: string = setValues[setValues.length - 1].trim();
         let filtered = values.filter((value: string) => value.indexOf(lastSetValue) !== -1);
         if (filtered.length !== 0) {
-            return AutocompletePipe.addTriplePoints(filtered);
+            return filtered;
         }
         return ['No matches']
-    }
-
-    private static addTriplePoints(values: string[]): string[] {
-        if (values.length > AutocompletePipe.maxElements) {
-            return values.slice(0, AutocompletePipe.maxElements).concat([ SetComponent.dots ])
-        } else return values;
     }
 }
--- app/frontend/app/common/filters/common/set/set.component.ts
+++ app/frontend/app/common/filters/common/set/set.component.ts
@@ -1,25 +1,33 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 
 
 @Component({
-    selector: 'set',
-    templateUrl: './set.component.html',
+    selector:        'set',
+    templateUrl:     './set.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SetComponent {
-    static dots: string = '...';
-
-    @Input() model: string;
-    @Output() modelChange = new EventEmitter();
-
-    @Input() placeholder: string;
-
-    @Input() search: string[] = [];
-
     private _searchVisible: boolean = false;
 
-    get searchVisible() {
-        return this._searchVisible && this.search.length !== 0;
+    @ViewChild('input') input: ElementRef;
+
+    @Input()
+    model: string;
+
+    @Output()
+    modelChange = new EventEmitter();
+
+    @Input()
+    placeholder: string;
+
+    @Input()
+    search: string[] = [];
+
+    fakeModel: string = '';
+    selected: string[] = [];
+
+    focus() {
+        this.input.nativeElement.focus();
     }
 
     onFocusIn(): void {
@@ -30,37 +38,19 @@ export class SetComponent {
         this._searchVisible = false;
     }
 
-    change(newValue: string): void {
-        this.model = newValue;
-        this.modelChange.emit(newValue);
-
-        // if (this.allowSuggestions) {
-        //     this.availableSuggestions.splice(0, this.availableSuggestions.length);
-        //     let values = this.model.split(",");
-        //     this.haveSuggestions = false;
-        //     for (let i = 0; i < values.length; ++i) {
-        //         let value = values[ i ];
-        //         if (this.suggestions.hasOwnProperty(value)) {
-        //             let suggestionsForValue: any = this.suggestions[ value ];
-        //             for (let j = 0; j < suggestionsForValue.length; ++j) {
-        //                 let suggestion: any = suggestionsForValue[ j ];
-        //                 if (values.indexOf(suggestion.sequence) === -1) {
-        //                     this.availableSuggestions.push(suggestion);
-        //                     this.haveSuggestions = true;
-        //                 }
-        //             }
-        //         }
-        //     }
-        // }
+    change(): void {
+        this.model = this.selected.join(',');
+        if (this.fakeModel !== '') this.model += this.fakeModel;
+        this.modelChange.emit(this.model);
     }
 
     append(value: string): void {
-        if (value !== SetComponent.dots) {
-            let values = this.model.split(',');
-            values[ values.length - 1 ] = value;
-            let newValue = values.join(',');
-            this.change(newValue);
-        }
+        this.selected.push(value);
+        this.fakeModel = '';
+        this.change();
     }
 
+    get searchVisible() {
+        return this._searchVisible && this.search.length !== 0;
+    }
 }
