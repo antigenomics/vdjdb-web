64dff8e4b1276898e541b0fa5193b072039fadf2
bvdmitri <bvdmitri@gmail.com>
2017-09-21 14:47:51 +0300

    lint fix

--- app/frontend/app/common/table/search/search-table.module.ts
+++ app/frontend/app/common/table/search/search-table.module.ts
@@ -8,12 +8,12 @@ import { SearchTableEntryJsonComponent } from './entry/json/search-table-entry-j
 import { SearchTableEntryOriginalComponent } from './entry/original/search-table-entry-original.component';
 import { SearchTableEntryDirective } from './entry/search-table-entry.directive';
 import { SearchTableEntryUrlComponent } from './entry/url/search-table-entry-url.component';
+import { SearchTableExportComponent } from './export/search-table-export.component';
 import { SearchTableInfoComponent } from './info/search-table-info.component';
 import { SearchTablePaginationComponent } from './pagination/search-table-pagination.component';
 import { SearchTableRowComponent } from './row/search-table-row.component';
 import { SearchTableComponent } from './search-table.component';
 import { SearchTableService } from './search-table.service';
-import { SearchTableExportComponent } from './export/search-table-export.component';
 
 @NgModule({
     imports:         [ BrowserModule, FormsModule, ModalsModule ],
