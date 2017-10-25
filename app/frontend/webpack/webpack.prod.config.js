5e972b925e50906749f7b2db228e9ba3b598f77b
bvdmitri <bvdmitri@gmail.com>
2017-10-25 12:24:02 +0300

    better compression of css files

--- app/frontend/webpack/webpack.prod.config.js
+++ app/frontend/webpack/webpack.prod.config.js
@@ -1,8 +1,10 @@
-console.log('Configuring webpack in production mode');
+console.log('Running webpack in production mode');
 
 const path = require('path');
+const glob = require('glob-all');
 const webpack = require('webpack');
 const TSLintPlugin = require('tslint-webpack-plugin');
+const PurifyCSSPlugin = require('purifycss-webpack');
 
 const defaultConfiguration = require('./webpack.base.config');
 
@@ -16,12 +18,24 @@ defaultConfiguration.plugins.push(
     new TSLintPlugin({
         config: path.resolve(__dirname, '../tslint.json'),
         files: [ './app/**/*.ts' ],
-        exclude: [ './app/main.prod.ts' ],
+        exclude: [ './app/main.prod.ts', './app/build/**/*.ts' ],
         fix: false,
         project: path.resolve(__dirname, '../tsconfig.ngfactory.json'),
         typeCheck: true
     }),
-    new webpack.NoEmitOnErrorsPlugin()
+    new webpack.NoEmitOnErrorsPlugin(),
+    new PurifyCSSPlugin({
+        paths: glob.sync([
+            path.join(__dirname, '../views/**/*.html'),
+            path.join(__dirname, '../app/**/*.html'),
+            path.join(__dirname, '../lib/aot/build/**/*.js')
+        ]),
+        purifyOptions: {
+            info: true,
+            minify: true,
+            output: path.join(__dirname, '../../../public/bundles/bundle.min.css')
+        }
+    })
 );
 
 
