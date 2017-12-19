4e807593734f6128239f11cfb178125ccde358bb
bvdmitri <bvdmitri@gmail.com>
2017-12-19 19:48:59 +0300

    dummy d3 chart

--- app/frontend/src/app/shared/charts/bar/chart-horizontal-bar.component.ts
+++ app/frontend/src/app/shared/charts/bar/chart-horizontal-bar.component.ts
@@ -20,14 +20,42 @@ import * as d3 from 'external/d3';
 import { ChartComponent } from 'shared/charts/chart.component';
 
 @Component({
-    selector: 'chart-horizontal-bar',
-    template: '<div #chartContainer></div>'
+    selector:  'chart-horizontal-bar',
+    template:  '<div #chartContainer style="width: 100%; height: 400px"></div>',
+    styleUrls: [ './chart-bar.styles.css' ]
 })
 export class ChartHorizontalBarComponent extends ChartComponent implements AfterViewInit {
     @ViewChild('chartContainer', { read: ElementRef })
     public chartContainer: ElementRef;
 
     public ngAfterViewInit(): void {
-        d3.select(this.chartContainer.nativeElement);
+        const data = [ 23, 13, 21, 14, 37, 15, 18, 34, 30, 23, 13, 21, 14, 37, 15, 18, 34, 30 ];
+        const dataLength = data.length;
+        const { width, height } = this.chartContainer.nativeElement.getBoundingClientRect();
+
+        const wb = 10 * width / (11 * dataLength - 1);
+
+        const svg = d3.select(this.chartContainer.nativeElement)
+                      .append('svg')
+                      .attr('height', '100%')
+                      .attr('width', '100%');
+
+        svg.selectAll('rect')
+           .data(data)
+           .enter().append('rect')
+           .attr('class', 'bar')
+           .attr('height', (d) => d * 10)
+           .attr('width', wb)
+           .attr('x', (d, i) => i * (wb + wb / 10) + wb / 10)
+           .attr('y', (d, i) => height - (d * 10));
+
+        svg.selectAll('text')
+           .data(data)
+           .enter().append('text')
+           .text((d) => d)
+           .attr('class', 'text')
+           .attr('x', (d, i) => i * (wb + wb / 10) + wb / 2)
+           .attr('y', (d, i) => height + 15 - (d * 10));
+
     }
 }
