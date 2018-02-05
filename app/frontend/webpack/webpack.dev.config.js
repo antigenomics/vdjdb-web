c5007830afa620fd7bbd2300269b8ab54bacebf8
Dmitri Bagaev <bvdmitri@gmail.com>
2018-02-05 18:17:46 +0300

    separate polyfills for IE

--- app/frontend/webpack/webpack.dev.config.js
+++ app/frontend/webpack/webpack.dev.config.js
@@ -25,6 +25,7 @@ const { DllBundlesPlugin } = require('webpack-dll-bundles-plugin');
 const { TsConfigPathsPlugin, CheckerPlugin } = require('awesome-typescript-loader');
 const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 
+
 defaultConfiguration.entry[ 'bundle.js' ] = [
     'webpack-dev-server/client?http://localhost:8080',
     './src/main.dev.ts'
@@ -72,40 +73,7 @@ defaultConfiguration.plugins.push(new DllBundlesPlugin({
     dllDir: './webpack/dll/',
     webpackConfig: webpackMerge(defaultConfiguration, {
         devtool: false,
-        plugins: [ new UglifyJSPlugin({
-            parallel: true,
-            uglifyOptions: {
-                ie8: false,
-                warnings: true,
-                ecma: 5,
-                compress: {
-                    sequences: true,  // join consecutive statemets with the “comma operator”
-                    properties: true,  // optimize property access: a["foo"] → a.foo
-                    dead_code: true,  // discard unreachable code
-                    drop_debugger: true,  // discard “debugger” statements
-                    unsafe: false, // some unsafe optimizations (see below)
-                    conditionals: true,  // optimize if-s and conditional expressions
-                    comparisons: true,  // optimize comparisons
-                    evaluate: true,  // evaluate constant expressions
-                    booleans: true,  // optimize boolean expressions
-                    loops: true,  // optimize loops
-                    unused: true,  // drop unused variables/functions
-                    hoist_funs: true,  // hoist function declarations
-                    hoist_vars: false, // hoist variable declarations
-                    if_return: true,  // optimize if-s followed by return/continue
-                    join_vars: true,  // join var declarations
-                    cascade: true,  // try to cascade `right` into `left` in sequences
-                    side_effects: true,  // drop side-effect-free statements
-                    warnings: true,   // warn about potentially dangerous optimizations/code,
-                    inline: true,
-                    reduce_vars: true,
-                    passes: 10
-                },
-                mangle: {
-                    keep_fnames: true
-                }
-            }
-        }) ]
+        plugins: [ new UglifyJSPlugin(UglifyJSDefaultConfiguration) ]
     })
 }));
 
