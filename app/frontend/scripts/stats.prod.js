c707d4502c9039fde7ed2f023c92b0ed38cf06f9
Dmitri Bagaev <bvdmitri@gmail.com>
2018-02-05 18:41:37 +0300

    to prev

--- app/frontend/scripts/stats.prod.js
+++ app/frontend/scripts/stats.prod.js
@@ -23,14 +23,6 @@ const path = require('path');
 const pathToBundle = path.resolve(__dirname, '../../../public/bundles/');
 const gzipSize = require('gzip-size');
 
-function getStats(fileName) {
-    const stats = fs.statSync(pathToBundle + '/' + fileName);
-    const size = (stats.size / 1024.0).toFixed(2);
-    const gzip = (gzipSize.sync(fs.readFileSync(pathToBundle + '/' + fileName)) / 1024.0).toFixed(2);
-
-    return {fileName: fileName, size: size, gzip: gzip}
-}
-
 if (fs.existsSync(pathToBundle + '/bundle.css')) {
     fs.unlinkSync(pathToBundle + '/bundle.css');
 }
@@ -85,26 +77,4 @@ const bundleStats = bundleFiles.map((file, index) => {
     },
 ]);
 
-
 console.table('Frontend bundle statistics', bundleStats);
-//
-// let total = { size: 0, gzip: 0 };
-//
-// const bundleStats = bundleFiles
-//     .map(function (fileName, index) {
-//         const stats = getStats(fileName);
-//         total.size += parseFloat(stats.size);
-//         total.gzip += parseFloat(stats.gzip);
-//         return {
-//             '#': index + 1,
-//             'File name': stats.fileName,
-//             'Size': stats.size += ' KB',
-//             'gzip': stats.gzip += ' KB'
-//         }
-//     })
-//     .concat([
-//         { '#': '', 'File name': '================', 'Size': '===========', 'gzip': '===========' },
-//         { '#': '', 'File name': '', 'Size': total.size.toFixed(2) + ' KB', 'gzip': total.gzip.toFixed(2) + ' KB' }
-//     ]);
-//
-// console.table('Fronted bundle statistics', bundleStats);
